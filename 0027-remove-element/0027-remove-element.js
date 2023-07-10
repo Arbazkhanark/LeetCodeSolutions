45a091e2bf70cf3739393802dc343f9b709f284a
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2023-07-10 13:58:28 +0530

    Time: 82 ms (5.17%), Space: 44.8 MB (5.37%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let index = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+
+    // Set the length of the array to the new index
+    nums.length = index;  
+
+    console.log(nums)
+    // return nums.length; 
+};
